14748966bda4fbbabddc8fd558ffcad97ad81da2
Reddy, Visweswara | RIEPL <ganganakunta.reddy@rakuten.com>
2022-09-28 21:44:21 +0530

    adding calculator function

---
+++ vars/calculator.groovy
@@ -0,0 +1,9 @@
+def add(x,y){
+    echo "Sum of ${x} and ${y} is ${x+y}"
+}
+def multiply(x,y){
+    echo "Multiply of ${x} and ${y} is ${x*y}"
+}
+def substract(x,y){
+    echo "substract of ${x} and ${y} is ${x-y}"
+}
